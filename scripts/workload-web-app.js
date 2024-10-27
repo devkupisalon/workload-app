eb85e08b2b07af2022af73e8eaea7b9999834709
aekozhevnikov <dev.aykozhevnikov@gmail.com>
2024-10-27 23:15:53 +0500

    initial commit

--- scripts/workload-web-app.js
+++ scripts/workload-web-app.js
@@ -56,6 +56,7 @@ class WorkWebApp {
 
     async get_responsible(user_id) {
         const values = await this.processor.get_data(this.id, this.config);
+        console.log(values);
         const responsible = values.find(r => r[1].includes(user_id) || r[1] === user_id);
     
         console.log(responsible);
